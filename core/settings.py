3bed76216acd5d973cc9ab37abaa2d0df5873112
Gucci <adkhamjonmakmudov@gmail.com>
2023-01-24 21:39:52 +0500

    add sentry

--- core/settings.py
+++ core/settings.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 import os
 import environ
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 env = environ.Env(
     # set casting, default value
@@ -106,3 +108,19 @@ MEDIA_ROOT = os.path.join(BASE_DIR / 'media')
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+sentry_sdk.init(
+    dsn=env('DNS'),
+    integrations=[
+        DjangoIntegration(),
+    ],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
