2d9568edb66a3010e740ac08b92f58b107580b3f
Ahror <uzkod136@mail.ru>
2023-01-26 03:02:05 +0500

    edit models and edit gitignore file

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
 from core import settings
 
 schema_view = get_schema_view(
@@ -11,9 +12,6 @@ schema_view = get_schema_view(
         title="ModMe API",
         default_version='v1',
         description="Backend API for ModMe",
-        terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="modme@service"),
-        license=openapi.License(name="BSD License"),
     ),
     public=True,
     permission_classes=[permissions.AllowAny],
@@ -22,6 +20,10 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('', include('apps.urls')),
     path('', include('users.urls')),
+    path('', include('crm.urls')),
+    path('', include('payments.urls')),
     path('admin/', admin.site.urls),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- crm/urls.py
+++ crm/urls.py
@@ -1,14 +1,2 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-
-from users.views import UserModelViewSet
-
-router = DefaultRouter()
-router.register('user', UserModelViewSet, basename='user')
-
 urlpatterns = [
-    path('', include(router.urls)),
-    path('token', TokenObtainPairView.as_view(), name='get_token'),
-    path('token/refresh', TokenRefreshView.as_view(), name='refresh_token'),
 ]
