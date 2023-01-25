6133426bfbfa184cc3fbe71c8d01a115b416319a
Ahror <uzkod136@mail.ru>
2023-01-25 18:49:09 +0500

    model bug fixed

--- users/managers.py
+++ users/managers.py
@@ -4,20 +4,20 @@ from django.contrib.auth.models import UserManager
 
 class MyUserManager(UserManager):
 
-    def _create_user(self, phone, email, password=None, **extra_fields):
+    def _create_user(self, phone, password=None, **extra_fields):
         if not phone:
             raise ValueError("The given phone number must be set")
-        user = self.model(phone=phone, email=email, **extra_fields)
+        user = self.model(phone=phone, **extra_fields)
         user.password = make_password(password)
         user.save(using=self._db)
         return user
 
-    def create_user(self, phone, email=None, password=None, **extra_fields):
+    def create_user(self, phone, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", False)
         extra_fields.setdefault("is_superuser", False)
-        return self._create_user(phone, email, password, **extra_fields)
+        return self._create_user(phone, password, **extra_fields)
 
-    def create_superuser(self, phone, email=None, password=None, **extra_fields):
+    def create_superuser(self, phone, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
 
@@ -26,4 +26,4 @@ class MyUserManager(UserManager):
         if extra_fields.get("is_superuser") is not True:
             raise ValueError("Superuser must have is_superuser=True.")
 
-        return self._create_user(phone, email, password, **extra_fields)
+        return self._create_user(phone, password, **extra_fields)
