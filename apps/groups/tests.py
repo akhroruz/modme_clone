9b6a06c62ccc54dba5c2bb877cadc4f799455c86
Javlon Baxtiyorov <baxtiyorovjavlon8@gmail.com>
2023-01-26 22:19:05 +0500

    testing role

--- apps/groups/tests.py
+++ apps/groups/tests.py
@@ -1,2 +1,20 @@
+import pytest
 from django.test import TestCase
 
+from groups.models import Role
+
+
+@pytest.mark.django_db
+class TestRoleModel:
+
+    @pytest.fixture
+    def role(self):
+        role = Role.objects.create(name='Role 1')
+        return role
+
+    def test_role_create_model(self):  # model
+        name = 'Role 2'
+        count = Role.objects.count()
+        role = Role.objects.create(name=name)
+        assert role.name == name
+        assert Role.objects.count() - 1 == count
