612a14537b00415ebfa2c9bcb62c0d0419993a07
Javlon Baxtiyorov <baxtiyorovjavlon8@gmail.com>
2023-01-30 17:57:17 +0500

    change password

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from apps.users.views import UserModelViewSet, RegisterView
+from apps.users.views import RegisterView, ChangePasswordView
 
 router = DefaultRouter()
 # router.register('user', UserModelViewSet, basename='user')
@@ -12,4 +12,5 @@ urlpatterns = [
     path('auth/register', RegisterView.as_view(), name='register'),
     path('auth/token', TokenObtainPairView.as_view(), name='get_token'),
     path('auth/refresh-token', TokenRefreshView.as_view(), name='refresh_token'),
+    path('auth/change_password/<str:uuid>', ChangePasswordView.as_view(), name='change_password'),
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,10 +1,10 @@
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, UpdateAPIView
 from rest_framework.parsers import MultiPartParser
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 from apps.users.models import User
-from apps.users.serializers import RegisterSerializer, UserModelSerializer
+from apps.users.serializers import RegisterSerializer, UserModelSerializer, ChangePasswordSerializer
 
 
 class UserModelViewSet(ModelViewSet):
@@ -17,3 +17,10 @@ class RegisterView(CreateAPIView):
     queryset = User.objects.all()
     permission_classes = (AllowAny,)
     serializer_class = RegisterSerializer
+
+
+class ChangePasswordView(UpdateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (IsAuthenticated,)
+    serializer_class = ChangePasswordSerializer
+
