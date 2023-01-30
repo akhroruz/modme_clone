5e8a26052f4fde01aa9b5eb03908ce697c5682df
Ahror <uzkod136@mail.ru>
2023-01-30 18:15:19 +0500

    add register api and tests

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from apps.users.views import RegisterView
-from users.views import StudentModelViewSet
+from apps.users.views import StudentModelViewSet
 
 router = DefaultRouter()
 router.register('student', StudentModelViewSet, basename='student')
