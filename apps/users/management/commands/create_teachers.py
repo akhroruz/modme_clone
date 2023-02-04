9a034961720272bbd6f0ee2e8c0c0488bc85ffde
m1rsd <mirsaidavazbekov@gmail.com>
2023-02-04 14:51:50 +0500

    Create Commands

--- apps/users/management/commands/create_teachers.py
+++ apps/users/management/commands/create_teachers.py
@@ -1,10 +1,14 @@
+from itertools import cycle
+
+from django.contrib.auth.models import Group
 from django.core.management import BaseCommand
 from faker import Faker
-from faker.utils.text import slugify
-
-from apps.groups.models import Role
-from apps.users.models import User
 from model_bakery import baker
+
+from groups.models import Branch
+from users.models import User
+
+faker = Faker()
 
 
 class Command(BaseCommand):
@@ -15,17 +19,18 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         total = options.get('total')
-        faker = Faker()
-        # role = Role.objects.filter(name='Teacher')
+        role = Group.objects.filter(name='teacher')
+        branch = Branch.objects.all()
         baker.make(
             'users.User',
-            first_name=faker.first_name(),
-            last_name=faker.last_name(),
-            birth=faker.date_of_birth(),
-            phone=faker.random_number(digits=9, fix_len=False),
+            first_name=cycle(faker.first_name()),
+            last_name=cycle(faker.last_name()),
+            birth_date=faker.date_of_birth(),
+            phone=faker.random_number(digits=9),
             gender=faker.random_element(User.GenderChoose),
-            # role=role,
-            make_m2m=True,
+            role=role,
+            branch=cycle(faker.random_choices(branch)),
+            make_m2m=False,
             _quantity=total
         )
 
