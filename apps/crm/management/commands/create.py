7e338e16ece54dbf5f4b42c3f7c37c90bd52804e
Ahror <uzkod136@mail.ru>
2023-02-04 19:06:42 +0500

    add faker

--- apps/crm/management/commands/create.py
+++ apps/crm/management/commands/create.py
@@ -1,11 +1,14 @@
 import random
 from itertools import cycle
 
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import Group
 from django.core.management import BaseCommand
 from faker import Faker
 from model_bakery import baker
 
-from groups.models import Company, Branch, Room, Course
+from groups.models import Company, Branch
+from users.models import User, LeadIncrement
 
 fake = Faker()
 
@@ -24,6 +27,10 @@ class Command(BaseCommand):
         parser.add_argument('-r', '--room', type=int, help='Define a room number prefix', )
         parser.add_argument('-hd', '--holiday', type=int, help='Define a holiday number prefix', )
         parser.add_argument('-cg', '--coursegroup', type=int, help='Define a coursegroup number prefix', )
+        parser.add_argument('-u', '--user', type=int, help='Define a user number prefix', )
+        parser.add_argument('-a', '--archive', type=int, help='Define a archive number prefix', )
+        parser.add_argument('-li', '--lead_increment', type=int, help='Define a lead increment number prefix', )
+        parser.add_argument('-l', '--lead', type=int, help='Define a lead number prefix', )
 
     def handle(self, *args, **options):
         # companies
@@ -86,3 +93,53 @@ class Command(BaseCommand):
             _quantity=r
         )
         print(r, 'holidays is being addded')
+
+        # user
+        u = options.get('user', 15)
+        company_code = ('90', '99', '98', '93', '94')
+        baker.make(
+            'users.User',
+            first_name=cycle(fake.first_name() for _ in range(u)),
+            last_name=cycle(fake.last_name() for _ in range(u)),
+            birth_date=cycle(fake.date_of_birth(maximum_age=70, minimum_age=7) for _ in range(u)),
+            phone=cycle(random.choice(company_code) + str(fake.random_number(digits=7)).zfill(7) for _ in range(u)),
+            gender=cycle(fake.random_element(User.GenderChoose) for _ in range(100)),
+            role=Group.objects.filter(name='teacher'),
+            branch=cycle(fake.random_element(Branch.objects.all()) for _ in range(u)),
+            comment=cycle(fake.text() for _ in range(u)),
+            password=make_password('1'),
+            _quantity=u
+        )
+
+        print(c, 'users is being addded')
+
+        # archive
+        a = options.get('archive', 15)
+        baker.make(
+            'users.Archive',
+            name=cycle(fake.first_name() for _ in range(a)),
+            _quantity=a
+        )
+        print(a, 'archives is being addded')
+
+        # lead increment
+        li = options.get('lead_increment', 15)
+        baker.make(
+            'users.LeadIncrement',
+            name=cycle(fake.first_name() for _ in range(li)),
+            _quantity=li
+        )
+
+        print(li, 'lead increments is being addded')
+
+        # lead
+        l = options.get('lead', 15)
+        baker.make(
+            'users.Lead',
+            full_name=cycle(fake.first_name() for _ in range(l)),
+            comment=cycle(fake.text() for _ in range(l)),
+            phone=cycle(random.choice(company_code) + str(fake.random_number(digits=7)).zfill(7) for _ in range(u)),
+            lid_increment=cycle(LeadIncrement.objects.all()),
+            _quantity=l
+        )
+        print(l, 'lead is being addded')
