bd08ac1f83a1ae6258525849076c8d72a5f3ef78
Gucci <adkhamjonmakmudov@gmail.com>
2023-02-19 23:46:35 +0500

    FULL TESTS WORKIN 92% coverage

--- apps/groups/tests/test_view.py
+++ apps/groups/tests/test_view.py
@@ -1,191 +1,192 @@
-# import pytest
-# from django.core.files.uploadedfile import SimpleUploadedFile
-# from django.test import Client
-# from django.test.client import BOUNDARY, MULTIPART_CONTENT, encode_multipart  # noqa
-# from rest_framework import status
-# from rest_framework.reverse import reverse
-# from core.settings import MEDIA_ROOT
-# from groups.models import Branch, Company, Room
-# from users.models import User
-#
-#
-# @pytest.mark.django_db
-# class TestBranchModelViewSet:
-#
-#     @pytest.fixture
-#     def company(self):
-#         company = Company.objects.create(name='Company 1')
-#         return company
-#
-#     @pytest.fixture
-#     def branch(self, company):
-#         image_path = MEDIA_ROOT + '/img.png'
-#         image = SimpleUploadedFile('img.png', content=open(image_path, 'rb').read(), content_type='image/jpeg')
-#         branch = Branch.objects.create(
-#             name='Branch 1',
-#             address='Uzbekistan, Toshkent',
-#             phone='12345678',
-#             about='Smth about this branch',
-#             company=company,
-#             image=image
-#
-#         )
-#         return branch
-#
-#     def test_list_branch(self, client: Client, branch):
-#         url = '%s?company=%s' % (reverse('branch-list'), branch.company.pk)
-#         response = client.get(url)
-#         item = response.data['results'][0]
-#         # json.loads(response.content) == response.data     # ishlatib korish kere shunaqa stildi
-#         assert response.status_code == status.HTTP_200_OK
-#         assert item['name'] == branch.name
-#         assert item['address'] == branch.address
-#         assert item['phone'] == branch.phone
-#         assert item['about'] == branch.about
-#         assert item['company'] == branch.company.pk
-#
-#     def test_create_branch(self, client: Client, branch):
-#         url = '%s?company=%s' % (reverse('branch-list'), branch.company.id)
-#         image_path = MEDIA_ROOT + '/img.png'
-#         image = SimpleUploadedFile('img.png', content=open(image_path, 'rb').read(), content_type='image/jpeg')
-#         data = {
-#             'name': branch.name,
-#             'address': branch.address,
-#             'phone': '987654321',
-#             'about': branch.about,
-#             'company': branch.company.pk,
-#             'image': image
-#         }
-#         response = client.post(url, data)
-#         assert response.status_code == status.HTTP_201_CREATED
-#         item = response.json()
-#         assert item['name'] == data['name']
-#         assert item['address'] == data['address']
-#         assert item['phone'] == data['phone']
-#         assert item['about'] == data['about']
-#         assert item['company'] == data['company']
-#
-#     def test_retrieve_branch(self, client: Client, branch):
-#         url = '%s?company=%s' % (reverse('branch-detail', args=[branch.id]), branch.company.id)
-#         response = client.get(url)
-#         assert response.status_code == status.HTTP_200_OK
-#
-#         item = response.data
-#         assert item['name'] == branch.name
-#         assert item['address'] == branch.address
-#         assert item['phone'] == branch.phone
-#         assert item['about'] == branch.about
-#         assert item['company'] == branch.company.pk
-#
-#     def test_update_branch(self, client: Client, branch):
-#         url = '%s?company=%s' % (reverse('branch-detail', args=[branch.id]), branch.company.id)
-#         data = {
-#             'name': 'New updated Branch 1',
-#             'address': 'dsadas',
-#             'phone': '11111111',
-#             'about': branch.about,
-#             'company': branch.company.pk,
-#             'image': branch.image,
-#         }
-#         response = client.put(url, encode_multipart(BOUNDARY, data), MULTIPART_CONTENT)
-#         assert response.status_code == status.HTTP_200_OK
-#         item = response.data
-#         assert item['name'] == data['name']
-#         assert item['address'] == data['address']
-#         assert item['phone'] == data['phone']
-#         assert item['about'] == data['about']
-#         assert item['company'] == data['company']
-#
-#     def test_delete_branch(self, client: Client, branch):
-#         url = '%s?company=%s' % (reverse('branch-detail', args=[branch.id]), branch.company.id)
-#         response = client.delete(url)
-#         assert response.status_code == status.HTTP_204_NO_CONTENT
-#
-#
-# @pytest.mark.django_db
-# class TestRoomModelViewSet:
-#
-#     @pytest.fixture
-#     def user(self):
-#         user = User.objects.create_user(phone='123654987', password='Asdvbn12ghnf115dfsa2f', is_superuser=True,
-#                                         is_staff=True)
-#         return user
-#
-#     @pytest.fixture
-#     def company(self):
-#         company = Company.objects.create(name='Company 1')
-#         return company
-#
-#     @pytest.fixture
-#     def branch(self, company):
-#         image_path = MEDIA_ROOT + '/img.png'
-#         image = SimpleUploadedFile('img.png', content=open(image_path, 'rb').read(), content_type='image/jpeg')
-#         branch = Branch.objects.create(
-#             name='Room 1',
-#             address='Uzbekistan, Toshkent',
-#             phone='12345678',
-#             about='Smth about this branch',
-#             company=company,
-#             image=image
-#         )
-#         return branch
-#
-#     @pytest.fixture
-#     def room(self, branch):
-#         room = Room.objects.create(name='Room 1', branch=branch)
-#         return room
-#
-#     def test_list_room(self, client: Client, room, user):
-#         client.force_login(user)
-#         url = reverse('room-list')
-#         response = client.get(url)
-#         assert response.status_code == status.HTTP_200_OK
-#         item = response.data['results'][0]
-#         assert item['id'] == room.id
-#         assert item['name'] == room.name
-#         assert item['branch'] == room.branch.pk
-#
-#     def test_create_room(self, client: Client, room, user):
-#         client.force_login(user)
-#         url = reverse('room-list')
-#         data = {
-#             'name': 'Room 2',
-#             'branch': room.branch.pk,
-#         }
-#
-#         response = client.post(url, data, 'application/json')
-#         assert response.status_code == status.HTTP_201_CREATED
-#
-#         item = response.json()
-#         assert item['name'] == data['name']
-#         assert item['branch'] == data['branch']
-#
-#     def test_retrieve_room(self, client: Client, room, user):
-#         client.force_login(user)
-#         url = reverse('room-detail', args=[room.id])
-#         response = client.get(url)
-#         assert response.status_code == status.HTTP_200_OK
-#
-#         item = response.data
-#         assert item['name'] == room.name
-#         assert item['branch'] == room.branch.pk
-#
-#     def test_update_room(self, client: Client, room, user):
-#         client.force_login(user)
-#         url = reverse('room-detail', args=[room.id])
-#         data = {
-#             'name': 'New updated Room 1',
-#             'branch': room.branch.pk,
-#         }
-#         response = client.put(url, encode_multipart(BOUNDARY, data), MULTIPART_CONTENT)
-#         assert response.status_code == status.HTTP_200_OK
-#
-#         item = response.data
-#         assert item['name'] == data['name']
-#         assert item['branch'] == data['branch']
-#
-#     def test_delete_room(self, client: Client, room, user):
-#         client.force_login(user)
-#         url = reverse('room-detail', args=[room.id])
-#         response = client.delete(url)
-#         assert response.status_code == status.HTTP_204_NO_CONTENT
+import pytest
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.test import Client
+from django.test.client import BOUNDARY, MULTIPART_CONTENT, encode_multipart  # noqa
+from rest_framework import status
+from rest_framework.reverse import reverse
+
+from core.settings import MEDIA_ROOT
+from groups.models import Branch, Company, Room
+from users.models import User
+
+
+@pytest.mark.django_db
+class TestBranchModelViewSet:
+
+    @pytest.fixture
+    def company(self):
+        company = Company.objects.create(name='Company 1')
+        return company
+
+    @pytest.fixture
+    def branch(self, company):
+        image_path = MEDIA_ROOT + '/test.png'
+        image = SimpleUploadedFile('test.png', content=open(image_path, 'rb').read(), content_type='image/jpeg')
+        branch = Branch.objects.create(
+            name='Branch 1',
+            address='Uzbekistan, Toshkent',
+            phone='12345678',
+            about='Smth about this branch',
+            company=company,
+            image=image
+
+        )
+        return branch
+
+    def test_list_branch(self, client: Client, branch):
+        url = '%s?company=%s' % (reverse('branch-list'), branch.company.pk)
+        response = client.get(url)
+        item = response.data['results'][0]
+        # json.loads(response.content) == response.data     # ishlatib korish kere shunaqa stildi
+        assert response.status_code == status.HTTP_200_OK
+        assert item['name'] == branch.name
+        assert item['address'] == branch.address
+        assert item['phone'] == branch.phone
+        assert item['about'] == branch.about
+        assert item['company'] == branch.company.pk
+
+    def test_create_branch(self, client: Client, branch):
+        url = '%s?company=%s' % (reverse('branch-list'), branch.company.id)
+        image_path = MEDIA_ROOT + '/test.png'
+        image = SimpleUploadedFile('test.png', content=open(image_path, 'rb').read(), content_type='image/jpeg')
+        data = {
+            'name': branch.name,
+            'address': branch.address,
+            'phone': '987654321',
+            'about': branch.about,
+            'company': branch.company.pk,
+            'image': image
+        }
+        response = client.post(url, data)
+        assert response.status_code == status.HTTP_201_CREATED
+        item = response.json()
+        assert item['name'] == data['name']
+        assert item['address'] == data['address']
+        assert item['phone'] == data['phone']
+        assert item['about'] == data['about']
+        assert item['company'] == data['company']
+
+    def test_retrieve_branch(self, client: Client, branch):
+        url = '%s?company=%s' % (reverse('branch-detail', args=[branch.id]), branch.company.id)
+        response = client.get(url)
+        assert response.status_code == status.HTTP_200_OK
+
+        item = response.data
+        assert item['name'] == branch.name
+        assert item['address'] == branch.address
+        assert item['phone'] == branch.phone
+        assert item['about'] == branch.about
+        assert item['company'] == branch.company.pk
+
+    def test_update_branch(self, client: Client, branch):
+        url = '%s?company=%s' % (reverse('branch-detail', args=[branch.id]), branch.company.id)
+        data = {
+            'name': 'New updated Branch 1',
+            'address': 'dsadas',
+            'phone': '11111111',
+            'about': branch.about,
+            'company': branch.company.pk,
+            'image': branch.image,
+        }
+        response = client.put(url, encode_multipart(BOUNDARY, data), MULTIPART_CONTENT)
+        assert response.status_code == status.HTTP_200_OK
+        item = response.data
+        assert item['name'] == data['name']
+        assert item['address'] == data['address']
+        assert item['phone'] == data['phone']
+        assert item['about'] == data['about']
+        assert item['company'] == data['company']
+
+    def test_delete_branch(self, client: Client, branch):
+        url = '%s?company=%s' % (reverse('branch-detail', args=[branch.id]), branch.company.id)
+        response = client.delete(url)
+        assert response.status_code == status.HTTP_204_NO_CONTENT
+
+
+@pytest.mark.django_db
+class TestRoomModelViewSet:
+
+    @pytest.fixture
+    def user(self):
+        user = User.objects.create_user(phone='123654987', password='Asdvbn12ghnf115dfsa2f', is_superuser=True,
+                                        is_staff=True)
+        return user
+
+    @pytest.fixture
+    def company(self):
+        company = Company.objects.create(name='Company 1')
+        return company
+
+    @pytest.fixture
+    def branch(self, company):
+        image_path = MEDIA_ROOT + '/test.png'
+        image = SimpleUploadedFile('test.png', content=open(image_path, 'rb').read(), content_type='image/jpeg')
+        branch = Branch.objects.create(
+            name='Room 1',
+            address='Uzbekistan, Toshkent',
+            phone='12345678',
+            about='Smth about this branch',
+            company=company,
+            image=image
+        )
+        return branch
+
+    @pytest.fixture
+    def room(self, branch):
+        room = Room.objects.create(name='Room 1', branch=branch)
+        return room
+
+    def test_list_room(self, client: Client, room, user):
+        client.force_login(user)
+        url = reverse('room-list')
+        response = client.get(url)
+        assert response.status_code == status.HTTP_200_OK
+        item = response.data['results'][0]
+        assert item['id'] == room.id
+        assert item['name'] == room.name
+        assert item['branch'] == room.branch.pk
+
+    def test_create_room(self, client: Client, room, user):
+        client.force_login(user)
+        url = reverse('room-list')
+        data = {
+            'name': 'Room 2',
+            'branch': room.branch.pk,
+        }
+
+        response = client.post(url, data, 'application/json')
+        assert response.status_code == status.HTTP_201_CREATED
+
+        item = response.json()
+        assert item['name'] == data['name']
+        assert item['branch'] == data['branch']
+
+    def test_retrieve_room(self, client: Client, room, user):
+        client.force_login(user)
+        url = reverse('room-detail', args=[room.id])
+        response = client.get(url)
+        assert response.status_code == status.HTTP_200_OK
+
+        item = response.data
+        assert item['name'] == room.name
+        assert item['branch'] == room.branch.pk
+
+    def test_update_room(self, client: Client, room, user):
+        client.force_login(user)
+        url = reverse('room-detail', args=[room.id])
+        data = {
+            'name': 'New updated Room 1',
+            'branch': room.branch.pk,
+        }
+        response = client.put(url, encode_multipart(BOUNDARY, data), MULTIPART_CONTENT)
+        assert response.status_code == status.HTTP_200_OK
+
+        item = response.data
+        assert item['name'] == data['name']
+        assert item['branch'] == data['branch']
+
+    def test_delete_room(self, client: Client, room, user):
+        client.force_login(user)
+        url = reverse('room-detail', args=[room.id])
+        response = client.delete(url)
+        assert response.status_code == status.HTTP_204_NO_CONTENT
--- apps/users/tests/test_model.py
+++ apps/users/tests/test_model.py
@@ -1,99 +1,99 @@
-# import pytest
-#
-# from groups.models import Company
-# from users.models import Archive, LeadIncrement, Lead, Blog, User
-#
-#
-# @pytest.mark.django_db
-# class TestArchiveModel:
-#     @pytest.fixture
-#     def archive(self):
-#         archive = Archive.objects.create(name='PDP')
-#         return archive
-#
-#     def test_archive(self, archive):
-#         assert archive.name == 'PDP'
-#         assert str(archive) == archive.name
-#
-#
-# @pytest.mark.django_db
-# class TestLeadIncrementModel:
-#     @pytest.fixture
-#     def lead_increment(self):
-#         lead_increment = LeadIncrement.objects.create(
-#             name='test_name'
-#         )
-#         return lead_increment
-#
-#     def test_lead_increment(self, lead_increment):
-#         assert lead_increment.name == 'test_name'
-#         assert str(lead_increment) == lead_increment.name
-#
-#
-# @pytest.mark.django_db
-# class TestLeadModel:
-#     @pytest.fixture
-#     def lead_increment(self):
-#         lead_increment = LeadIncrement.objects.create(
-#             name='test_name'
-#         )
-#         return lead_increment
-#
-#     @pytest.fixture
-#     def lead(self, lead_increment):
-#         lead = Lead.objects.create(
-#             full_name='test_fullname',
-#             comment='test_comment',
-#             phone=934492123,
-#             status='Requests',
-#             lead_increment=lead_increment
-#         )
-#         return lead
-#
-#     def test_lead(self, lead, lead_increment):
-#         assert lead.full_name == 'test_fullname'
-#         assert lead.comment == 'test_comment'
-#         assert lead.phone == 934492123
-#         assert lead.status == 'Requests'
-#         assert lead.lead_increment == lead_increment
-#         assert str(lead) == f'{lead.full_name} | {lead.phone}'
-#
-#
-# @pytest.mark.django_db
-# class TestBlogModel:
-#     @pytest.fixture
-#     def company(self):
-#         company = Company.objects.create(
-#             name='test_company'
-#         )
-#         return company
-#
-#     @pytest.fixture
-#     def user(self):
-#         user = User.objects.create_user(phone=1234567, password='pass')
-#         return user
-#
-#     @pytest.fixture
-#     def blog(self, company, user):
-#         blog = Blog.objects.create(
-#             title='test_title',
-#             text='test_text',
-#             public=True,
-#             created_by=user,
-#             updated_by=user,
-#             visible_all=True,
-#             view_count=100,
-#             company=company
-#         )
-#         return blog
-#
-#     def test_blog(self, blog, company, user):
-#         assert blog.title == 'test_title'
-#         assert blog.text == 'test_text'
-#         assert blog.public == True
-#         assert blog.created_by == user
-#         assert blog.updated_by == user
-#         assert blog.visible_all == True
-#         assert blog.view_count == 100
-#         assert blog.company == company
-#         assert str(blog) == blog.title
+import pytest
+
+from groups.models import Company
+from users.models import Archive, LeadIncrement, Lead, Blog, User
+
+
+@pytest.mark.django_db
+class TestArchiveModel:
+    @pytest.fixture
+    def archive(self):
+        archive = Archive.objects.create(name='PDP')
+        return archive
+
+    def test_archive(self, archive):
+        assert archive.name == 'PDP'
+        assert str(archive) == archive.name
+
+
+@pytest.mark.django_db
+class TestLeadIncrementModel:
+    @pytest.fixture
+    def lead_increment(self):
+        lead_increment = LeadIncrement.objects.create(
+            name='test_name'
+        )
+        return lead_increment
+
+    def test_lead_increment(self, lead_increment):
+        assert lead_increment.name == 'test_name'
+        assert str(lead_increment) == lead_increment.name
+
+
+@pytest.mark.django_db
+class TestLeadModel:
+    @pytest.fixture
+    def lead_increment(self):
+        lead_increment = LeadIncrement.objects.create(
+            name='test_name'
+        )
+        return lead_increment
+
+    @pytest.fixture
+    def lead(self, lead_increment):
+        lead = Lead.objects.create(
+            full_name='test_fullname',
+            comment='test_comment',
+            phone=934492123,
+            status='Requests',
+            lead_increment=lead_increment
+        )
+        return lead
+
+    def test_lead(self, lead, lead_increment):
+        assert lead.full_name == 'test_fullname'
+        assert lead.comment == 'test_comment'
+        assert lead.phone == 934492123
+        assert lead.status == 'Requests'
+        assert lead.lead_increment == lead_increment
+        assert str(lead) == f'{lead.full_name} | {lead.phone}'
+
+
+@pytest.mark.django_db
+class TestBlogModel:
+    @pytest.fixture
+    def company(self):
+        company = Company.objects.create(
+            name='test_company'
+        )
+        return company
+
+    @pytest.fixture
+    def user(self):
+        user = User.objects.create_user(phone=1234567, password='pass')
+        return user
+
+    @pytest.fixture
+    def blog(self, company, user):
+        blog = Blog.objects.create(
+            title='test_title',
+            text='test_text',
+            public=True,
+            created_by=user,
+            updated_by=user,
+            visible_all=True,
+            view_count=100,
+            company=company
+        )
+        return blog
+
+    def test_blog(self, blog, company, user):
+        assert blog.title == 'test_title'
+        assert blog.text == 'test_text'
+        assert blog.public == True
+        assert blog.created_by == user
+        assert blog.updated_by == user
+        assert blog.visible_all == True
+        assert blog.view_count == 100
+        assert blog.company == company
+        assert str(blog) == blog.title
