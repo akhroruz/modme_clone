5abb18e99081e34245a1ec659f282858ee876f9d
m1rsd <mirsaidavazbekov@gmail.com>
2023-02-21 14:52:10 +0500

    add tests to company model view set

--- apps/groups/tests/test_view.py
+++ apps/groups/tests/test_view.py
@@ -6,13 +6,12 @@ from rest_framework import status
 from rest_framework.reverse import reverse
 
 from core.settings import MEDIA_ROOT
-from groups.models import Branch, Company, Room
+from groups.models import Branch, Company, Room, Course
 from users.models import User
 
 
 @pytest.mark.django_db
 class TestBranchModelViewSet:
-
 
     @pytest.fixture
     def company(self):
@@ -190,5 +189,99 @@ class TestRoomModelViewSet:
     def test_delete_room(self, client: Client, room, user):
         client.force_login(user)
         url = reverse('room-detail', args=[room.id])
+        response = client.delete(url)
+        assert response.status_code == status.HTTP_204_NO_CONTENT
+
+
+@pytest.mark.django_db
+class TestHomeListAPIViewSet:
+    @pytest.fixture
+    def company(self):
+        company = Company.objects.create(name='Company 1')
+        return company
+
+    @pytest.fixture
+    def course(self, company):
+        image_path = MEDIA_ROOT + '/img.png'
+        image = SimpleUploadedFile('img.png', content=open(image_path, 'rb').read(), content_type='image/jpeg')
+        course = Course.objects.create(
+            name='Python Back End course',
+            price=1400000,
+            description='Smth about this course',
+            image=image,
+            lesson_duration=15,
+            course_duration=140,
+            company=company,
+        )
+        return course
+
+    def test_list_home(self, client: Client, course):
+        url = reverse('home')
+        response = client.get(url)
+        assert response.status_code == status.HTTP_200_OK
+        item = response.data['results'][0]
+        assert item['name'] == course.name
+        assert float(item['price']) == course.price  # problem , demical 2 ta nol qoshilib qolib qolyapti
+        assert item['description'] == course.description
+        # assert item['image'] == course.image  # xoto chiqyapti image di item da server url bilan kelyapti
+        assert item['lesson_duration'] == course.lesson_duration
+        assert item['course_duration'] == course.course_duration
+        assert item['company'] == course.company.pk
+
+
+@pytest.mark.django_db
+class TestCompanyModelViewSet:
+    @pytest.fixture
+    def user(self):
+        user = User.objects.create(phone='12345678', password='ASdgashjgjh3123', is_superuser=True, is_staff=True)
+        return user
+
+    @pytest.fixture
+    def company(self):
+        company = Company.objects.create(name='Company 1')
+        return company
+
+    def test_list_company(self, client: Client, company, user):
+        client.force_login(user)
+        url = reverse('company-list')
+        response = client.get(url)
+        assert response.status_code == status.HTTP_200_OK
+
+        item = response.data['results'][0]
+        assert item['name'] == company.name
+
+    def test_create_company(self, client: Client, company, user):
+        client.force_login(user)
+        url = reverse('company-list')
+        data = {
+            'name': 'Company test 1'
+        }
+        response = client.post(url, data)
+        assert response.status_code == status.HTTP_201_CREATED
+        item = response.json()
+        assert item['name'] == data['name']
+
+    def test_retrieve_company(self, client: Client, company, user):
+        client.force_login(user)
+        url = reverse('company-detail', args=(company.id,))
+        response = client.get(url)
+        assert response.status_code == status.HTTP_200_OK
+        item = response.json()
+        assert item['name'] == company.name
+
+    def test_update_company(self, client: Client, company, user):
+        client.force_login(user)
+        url = reverse('company-detail', args=(company.id,))
+        data = {
+            'name': 'Updated new name'
+        }
+        response = client.put(url, data, "application/json")
+        assert response.status_code == status.HTTP_200_OK
+        item = response.json()
+        assert item['name'] == data['name']
+
+    def test_delete_company(self, client: Client, company, user):
+        client.force_login(user)
+        url = reverse('company-detail', args=(company.id,))
         response = client.delete(url)
         assert response.status_code == status.HTTP_204_NO_CONTENT
